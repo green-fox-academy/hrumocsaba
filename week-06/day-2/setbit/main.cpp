d0f1843a14ff92e81428cad81a5c89b9d56fdc81
hrumocsaba <hrumos3@gmail.com>
2019-10-22 13:36:13 +0200

    added rotate left and right

--- week-06/day-2/setbit/main.cpp
+++ week-06/day-2/setbit/main.cpp
@@ -42,32 +42,69 @@ uint8_t is_even(uint8_t inp_byte){
     uint8_t inp_byte2 = inp_byte;
     uint8_t bitmask = 0b00000001;
     inp_byte = inp_byte | bitmask;
-    printf("%d %d \n", inp_byte, inp_byte2);
     inp_byte2 = inp_byte2 ^ inp_byte;
     return inp_byte2;
-
 }
+
 uint8_t is_odd(uint8_t inp_byte){
     uint8_t inp_byte2 = inp_byte;
     uint8_t bitmask = 0b00000001;
     inp_byte = inp_byte | bitmask;
-    printf("%d %d \n", inp_byte, inp_byte2);
     inp_byte2 = inp_byte2 ^ inp_byte;
     return inp_byte2 ^ bitmask;
+}
 
+uint8_t rotate_right(uint8_t inp_byte, int number){
+    uint8_t bitmask = 0b10000000;
+    uint8_t bitmask2 = ~bitmask;
+
+    for (int i = 0; i < number; ++i) {
+        if(is_odd(inp_byte)){
+            inp_byte = inp_byte >> 1;
+            inp_byte = inp_byte | bitmask;
+            PrintBinary(inp_byte);
+        }else{
+            inp_byte = inp_byte >> 1;
+            PrintBinary(inp_byte);
+        }
+    }
+    return inp_byte;
+}
+
+uint8_t rotate_left(uint8_t inp_byte, int number){
+    uint8_t bitmask = 0b00000001;
+    uint8_t bitmask2 = ~bitmask;
+
+    for (int i = 0; i < number; ++i) {
+        if((inp_byte & 0x80)){
+            inp_byte = inp_byte << 1;
+            inp_byte = inp_byte | bitmask;
+            PrintBinary(inp_byte);
+        }else{
+            inp_byte = inp_byte << 1;
+            PrintBinary(inp_byte);
+        }
+    }
+    return inp_byte;
 }
 
 
-int main() {
 
-       /* 1.
-     * Write a function called IsEven which takes a byte and returns with true
-     * if byte is even. Use only bitwise operators!
-     */
+int main() {
+    /* 1.
+ * Write a function called RotateRight which takes a byte and a number
+ * and rotates the bits of byte in right direction number times.
+ * It should return with the result.
+ * E.g. byte = 0b1000000, number = 1 => returns 0b01000000
+ * E.g. byte = 0b1000001, number = 2 => returns 0b01100000
+ */
 
     /* 2.
-     * Write a function called IsOdd which takes a byte and returns with true
-     * if byte is odd. Use only bitwise operators!
+     * Write a function called RotateLeft which takes a byte and a number
+     * and rotates the bits of byte in left direction number times.
+     * It should return with the result.
+     * E.g. byte = 0b1000000, number = 1 => returns 0b00000001
+     * E.g. byte = 0b1000001, number = 2 => returns 0b00000110
      */
 
     uint8_t byte = 0b0100101;
@@ -76,6 +113,11 @@ int main() {
     PrintBinary(toggle_bit(byte,2));
     uint8_t byte2 = 0b0001101;
     printf("%d \n", is_even(byte2));
-    printf("%d \n", is_odd(byte2));
+    printf("%d \n \n", is_odd(byte2));
+    PrintBinary(rotate_right(byte2, 4));
+    printf("\n");
+    PrintBinary(rotate_left(byte2, 2));
+
+
     return 0;
 }
