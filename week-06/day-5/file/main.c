ae45612b14864e0e89fb73bc54da396932313031
hrumocsaba <hrumos3@gmail.com>
2019-10-25 12:57:05 +0200

    done with line count

--- week-06/day-5/file/main.c
+++ week-06/day-5/file/main.c
@@ -11,10 +11,14 @@ int main ()
     FILE *fptr;
     fptr = fopen("../my-file.txt", "r");
     if(!(fptr = fopen("../my-file.txt", "r"))) return -1;
-    char c;
-    while ((c = fgetc(fptr)) > 0){
-        printf("%c",c);
+    char c[255];
+    int count = 0;
+    while ((fgets(c, 255, fptr)) > 0){
+        printf("%s",c);
+        count++;
     }
+
+    printf("\nthere are %d lines in this txt.", count);
     fclose(fptr);
     return 0;
 }
