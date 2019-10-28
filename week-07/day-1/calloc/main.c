336cfd86bb1afa33b675e97f98932e00a156e269
hrumocsaba <hrumos3@gmail.com>
2019-10-28 10:50:26 +0100

    done callloc

---
+++ week-07/day-1/calloc/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with calloc
+// please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
+// please delete the array before the program exits
+// what's the difference between this and the previous exercise?
+
+int main()
+{
+    int* pointer = calloc(10, sizeof(int));
+    for (int i = 0; i < 5; ++i) {
+        pointer[i]=i;
+    }
+    for (int j = 0; j < 10; ++j) {
+        printf("%d\n", pointer[j]);
+    }
+
+    return 0;
+}
